20af2d54fb2b87149a7abb16c432091b7daf14dc
Hammad <hammadkhan3923@gmail.com>
2022-06-22 01:40:42 -0700

    added more javascript

--- app.js
+++ app.js
@@ -11,11 +11,31 @@ const TestDisplay = document.getElementById("testScreen");
 
 //An example of me getting a element to than later apply a action to it. Number one is the exmaple here. Dont forget to add a ID to the html element
 const numberOneElement = document.getElementById("one");
+const numbertwoelement=document.getElementById("two");
+const numberthreeelement=document.getElementById("three");
+const numberfourelement=document.getElementById("four");
+const numberfiveelement=document.getElementById("five");
 //Apply a action to the element using a click in this instance.
 //My thought process is add the clicked element to the new display and add it to the array 
 const numberOneAction = numberOneElement.addEventListener("click", () => {
     inputStorage.push('1');
     TestDisplay.innerHTML = "<h1>1</h1>";
+});
+const numbertwoAction = numbertwoelement.addEventListener("click", () => {
+    inputStorage.push('2');
+    TestDisplay.innerHTML = "<h1>2</h1>";
+});
+const numberthreeAction = numberthreeelement.addEventListener("click", () => {
+    inputStorage.push('3');
+    TestDisplay.innerHTML = "<h1>3</h1>";
+});
+const numberfourAction = numberfourelement.addEventListener("click", () => {
+    inputStorage.push('4');
+    TestDisplay.innerHTML = "<h1>4</h1>";
+});
+const numberfiveAction = numberfiveelement.addEventListener("click", () => {
+    inputStorage.push('5');
+    TestDisplay.innerHTML = "<h1>5</h1>";
 });
 
 console.log(inputStorage);
