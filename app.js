b33da71ab147c0aa14f38a86a15dc71173c50c19
Hammad <hammadkhan3923@gmail.com>
2022-06-22 02:35:30 -0700

    some updation

--- app.js
+++ app.js
@@ -14,28 +14,48 @@ const numberOneElement = document.getElementById("one");
 const numbertwoelement=document.getElementById("two");
 const numberthreeelement=document.getElementById("three");
 const numberfourelement=document.getElementById("four");
-const numberfiveelement=document.getElementById("five");
+const numberfivelement=document.getElementById("five");
+const numbersixelement=document.getElementById("six");
+const numbersevenlement=document.getElementById("seven");
+const numbereightelement=document.getElementById("eight");
+const numbernineelement=document.getElementById("nine");
 //Apply a action to the element using a click in this instance.
 //My thought process is add the clicked element to the new display and add it to the array 
 const numberOneAction = numberOneElement.addEventListener("click", () => {
-    inputStorage.push('1');
-    TestDisplay.innerHTML = "<h1>1</h1>";
+    inputStorage.push('1'); 
+    calcDisplay.value='1';
 });
 const numbertwoAction = numbertwoelement.addEventListener("click", () => {
     inputStorage.push('2');
-    TestDisplay.innerHTML = "<h1>2</h1>";
+    calcDisplay.value='2';
 });
 const numberthreeAction = numberthreeelement.addEventListener("click", () => {
     inputStorage.push('3');
-    TestDisplay.innerHTML = "<h1>3</h1>";
+    calcDisplay.value='3';
 });
 const numberfourAction = numberfourelement.addEventListener("click", () => {
     inputStorage.push('4');
-    TestDisplay.innerHTML = "<h1>4</h1>";
+    calcDisplay.value='4';
 });
-const numberfiveAction = numberfiveelement.addEventListener("click", () => {
+const numberfiveAction = numberfivelement.addEventListener("click", () => {
     inputStorage.push('5');
-    TestDisplay.innerHTML = "<h1>5</h1>";
+    calcDisplay.value='5';
+});
+const numbersixAction = numbersixelement.addEventListener("click", () => {
+    inputStorage.push('6');
+    calcDisplay.value='6';
+});
+const numbersevenAction = numbersevenlement.addEventListener("click", () => {
+    inputStorage.push('7');
+    calcDisplay.value='7';
+});
+const numbereightAction = numbereightelement.addEventListener("click", () => {
+    inputStorage.push('8');
+    calcDisplay.value='8';
+});
+const numbernineAction = numbernineelement.addEventListener("click", () => {
+    inputStorage.push('9');
+    calcDisplay.value='9';
 });
 
 console.log(inputStorage);
